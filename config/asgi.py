b3e04323c299913b2045c3ff26eb586fb3b44928
Hruaia <hruaitluangasailo20@gmail.com>
2026-04-08 22:17:55 +0530

    fixsss

--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,18 @@
-from accounts.routing import websocket_urlpatterns as accounts_ws
-from forum.routing import websocket_urlpatterns as forum_ws
-import sessions_app.routing
-import livestream.routing
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+# isort: skip_file
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 django_asgi_app = get_asgi_application()
 
+import livestream.routing
+import sessions_app.routing
+from forum.routing import websocket_urlpatterns as forum_ws
+from accounts.routing import websocket_urlpatterns as accounts_ws
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
